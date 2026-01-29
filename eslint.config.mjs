fd1a4e64832c64ee36291f5dca70abf442f42f9b
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-29 14:44:12 +0800

    chore: migrate ESLint configuration to eslint.config.mjs and update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import globals from "globals";
+import js from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
+import reactNativePlugin from "eslint-plugin-react-native";
+import babelParser from "@babel/eslint-parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "react-native": reactNativePlugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
