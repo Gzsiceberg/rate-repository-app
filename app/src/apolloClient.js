71236f5e7f2abf43cf484b650eb41a96c78dd2ad
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-30 11:07:16 +0800

    fix: update GraphQL endpoint URI from localhost to local network address

--- app/src/apolloClient.js
+++ app/src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://192.168.5.10:4000/graphql',
   cache: new InMemoryCache(),
 });
 
