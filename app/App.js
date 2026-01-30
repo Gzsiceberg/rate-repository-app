e8a0dfb73320d37ec1703e27c276e5945969d695
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-30 11:05:41 +0800

    feat: integrate Apollo Client for GraphQL data fetching; implement useRepositories hook and related queries

--- app/App.js
+++ app/App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-
+import { ApolloProvider } from '@apollo/client';
 import { NativeRouter } from 'react-router-native';
 
 import Main from './src/components/Main';
+import apolloClient from './src/apolloClient';
 
 const App = () => {
   return (
     <>
-      <NativeRouter>
-        <Main />
-      </NativeRouter>
+      <ApolloProvider client={apolloClient}>
+        <NativeRouter>
+          <Main />
+        </NativeRouter>
+      </ApolloProvider>
       <StatusBar style="auto" />
     </>
   );
