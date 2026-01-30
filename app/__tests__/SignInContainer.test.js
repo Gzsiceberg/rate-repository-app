1de001ea51577564ff3b8d58833a5477bc1d6853
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-30 13:07:05 +0800

    test: update SignInContainer test to use consistent username and password values

--- app/__tests__/SignInContainer.test.js
+++ app/__tests__/SignInContainer.test.js
@@ -9,18 +9,18 @@ describe('SignIn', () => {
 
       render(<SignInContainer onSubmit={onSubmit} />);
 
-      fireEvent.changeText(screen.getByPlaceholderText('Username'), 'kalle');
+      fireEvent.changeText(screen.getByPlaceholderText('Username'), 'iceberg');
       fireEvent.changeText(
         screen.getByPlaceholderText('Password'),
-        'password'
+        'iceberg'
       );
       fireEvent.press(screen.getByText('Sign in'));
 
       await waitFor(() => {
         expect(onSubmit).toHaveBeenCalledTimes(1);
         expect(onSubmit.mock.calls[0][0]).toEqual({
-          username: 'kalle',
-          password: 'password',
+          username: 'iceberg',
+          password: 'iceberg',
         });
       });
     });
